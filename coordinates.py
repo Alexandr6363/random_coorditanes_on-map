9d91189cb142597384c4a08de20cc3b5cdb99894
AlexR <0130200@gmail.com>
2022-10-18 18:31:25 +0300

    get coordinate in format google maps link

--- coordinates.py
+++ coordinates.py
@@ -5,17 +5,43 @@ Arr2 = ["E", "W"]
 
 
 class Coordinate:
-    def __init__(self):
-        self.meridian = round(random.uniform(0, 180), 5)
-        self.side_SN = random.choice(Arr1)
-        self.palallels = round(random.uniform(0, 90), 5)
-        self.side_EW = random.choice(Arr2)
+        def __init__(self):
+            self.meridian_degrees = random.randint(0, 180)
+            self.meridian_minutes = random.randint(0, 60)
+            self.meridian_seconds = round(random.uniform(0, 60), 2)
+            self.side_SN = random.choice(Arr1)
+            self.parallels_degrees = random.randint(0, 90)
+            self.parallels_minutes = random.randint(0, 60)
+            self.parallels_seconds = round(random.uniform(0, 60), 2)
+            self.side_EW = random.choice(Arr2)
 
-    def __str__(self):
-        return f"{self.side_SN} {str(self.palallels)} " \
-               f"{self.side_EW} {str(self.meridian)}"
+        def __str__(self):
+            return f"https://www.google.com/maps/place/{self.parallels_degrees}%C2%B0{self.parallels_minutes}'{self.parallels_seconds}%22{self.side_SN}+" \
+                   f"{self.meridian_degrees}%C2%B0{self.meridian_minutes}'{self.meridian_seconds}%22{self.side_EW}/"
+
+        def get_meridian(self):
+            return f"{self.side_EW} {self.meridian_degrees} {self.meridian_minutes} {self.meridian_seconds}"
+
+        def get_parallel(self):
+            return f"{self.side_SN} {self.parallels_degrees} {self.parallels_minutes} {self.parallels_seconds}"
 
 
 list_of_cord = [Coordinate() for i in range(20)]
 for cord in list_of_cord:
     print(cord)
+
+"""
+https://www.google.com/maps/place/20%C2%B056'11.0%22S+55%C2%B024'03.6%22W/
+
+56° 34' 14.04'
+
+"""
+# return f"{self.side_EW} {self.parallels_degrees} {self.parallels_minutes} {self.parallels_seconds}" \
+#        f"{self.side_SN} {self.meridian_degrees} {self.meridian_minutes} {self.meridian_seconds}"
+
+
+
+
+
+
+
